6396e4f4b1f46b19372b2703c63e065934acc6e6
V. R <for_usenet@yahoo.com>
2022-03-09 20:08:54 -0500

    Adding simple example script for testing and prototyping.

---
+++ sample_shell_scripts_and_notes/Siemens/example_test.py
@@ -0,0 +1,20 @@
+
+import re, os, sys
+sys.path.insert(0, os.path.abspath('.'))
+import siemens_handler
+
+with open ('MrMeas_container.log.sample-2021-09-07', 'r') as raw_log:
+   log_lines = raw_log.readlines()
+
+sh = siemens_handler.log_parser()
+
+sh.find_event('MSR_OK',                      log_lines[30:])
+sh.find_event('MSR_STARTED',                 log_lines[30:])
+sh.find_event('MSR_SCANNER_FINISHED',        log_lines[30:])
+sh.find_event('MSR_ACQ_FINISHED',            log_lines[30:])
+sh.find_event('MSR_MEAS_FINISHED',           log_lines[30:])
+sh.find_event('EVENT_PATIENT_DEREGISTERED',  log_lines[30:])
+
+# Example of event that should NOT be in the logs
+sh.find_event('EVENT_PATIENT_REGISTERED',    log_lines[30:])
+
