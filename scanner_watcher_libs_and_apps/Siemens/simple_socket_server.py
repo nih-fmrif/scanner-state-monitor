a01be1daa31ba227166758e2c95a697f525124ec
V. R. <for_usenet@yahoo.com>
2026-04-10 09:25:24 -0400

    Fix simple_socket_server.py to confirm functionality - able to run this stand-alone now.

--- scanner_watcher_libs_and_apps/Siemens/simple_socket_server.py
+++ scanner_watcher_libs_and_apps/Siemens/simple_socket_server.py
@@ -7,9 +7,8 @@ import datetime
 
 
 
-HOST = "192.168.2.5"    # listening host for real time server
-PORT = 5000             # port on server real time is listening on
-FILE = "/tmp/.dcmRxInfo.log"
+HOST = "localhost"   # listening host for real time server
+PORT = 5000          # port on server real time is listening on
 
 
 
@@ -25,12 +24,11 @@ class simple_tcp_handler(socketserver.BaseRequestHandler):
 
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.server.cno = self.server.cno + 1
-        sys.stderr.write ("\n%s made a connection:\n" % self.client_address[0])
-        # print "Connection # %d:" % self.server.cno
+        self.server.connection_counter = self.server.connection_counter + 1
+        sys.stderr.write ("\n%s made a connection\n" % self.client_address[0])
 
-        # Get data and write it to the file    
-        while 1: 
+        # Get data and print ... could be modified as needed
+        while True:
             data = self.request.recv(1*1024*1024)
             if not data:
                 # no more data
@@ -40,7 +38,6 @@ class simple_tcp_handler(socketserver.BaseRequestHandler):
 
             if len(lines) > 0:
                 for each_line in lines:
-                    dcmMsgPipe = open(FILE, 'a')
 
                     # Check for MEAS_ in message string, and if present, add
                     # current system date and time to that line.
@@ -49,19 +46,12 @@ class simple_tcp_handler(socketserver.BaseRequestHandler):
                         # Siemens' logs, i.e. date: yyyy-mm-dd, and time:
                         # HH:MM:SS.milliseconds
                         current_time = datetime.datetime.now()
-                        dcmMsgPipe.write (current_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3] + ' ' + each_line + '\n')
+                        print(current_time.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3] + ' ' + each_line)
                     else:
-                        dcmMsgPipe.write (each_line + '\n')
-
-                    # These are needed only for files, not pipes
-                    # dcmMsgPipe.flush ()
-                    # os.fsync (dcmMsgPipe.fileno())
-
-                    # Close pipe/file
-                    dcmMsgPipe.close()
+                        print(each_line)
 
         # The connection has been closed
-        sys.stderr.write ("%s closed the connection:\n" % self.client_address[0])
+        sys.stderr.write ("%s closed the connection\n" % self.client_address[0])
 
 
 
@@ -89,5 +79,5 @@ if __name__ == "__main__":
       # this will keep running until you interrupt with Ctrl-C
       server.serve_forever()
    finally:
-      server.shutdown()
+      exit()
 
